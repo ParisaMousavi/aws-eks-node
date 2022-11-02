8c4db7d96f071b5c61647158d7328e1575d3ee59
Parisa Moosavinezhad <parisam@microsoft.com>
2022-11-02 15:17:47 +0100

    update

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "aws_eks_node_group" "this" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.this_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.this_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.this_AmazonEC2ContainerRegistryReadOnly,
   ]
   cluster_name    = var.cluster_name
   node_group_name = var.node_group_name
@@ -22,4 +22,38 @@ resource "aws_eks_node_group" "this" {
   lifecycle {
     ignore_changes = [scaling_config[0].desired_size]
   }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.name}-worker-role"
+  description        = "Allows EC2 instances to call AWS services on your behalf."
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "this_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "this_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "this_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.this.name
 }
